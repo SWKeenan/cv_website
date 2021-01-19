faecd1fce52c26fb780850c1d53eabd899ccc420
Shane Keenan <shanewilliamkeenan@gmail.com>
2021-01-19 21:15:36 +0800

    added buttons to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import loader from '../assets/images/loader.gif';
+import all from '../assets/images/all.svg';
+import wordpress from '../assets/images/wordpress.svg';
+import django from '../assets/images/django.svg';
+import react from '../assets/images/react.svg';
+import python from '../assets/images/python.svg';
+import github from '../assets/images/gh.svg';
+import java from '../assets/images/java.svg';
+import vue from '../assets/images/vue.svg';
 import './Projects.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -9,6 +17,17 @@ export default class Projects extends React.Component{
 
     state = {
         projects: [],
+        filteredProjects: [],
+        languagesButtons: [
+            {name: 'All', image: all},
+            {name: 'Python', image: python},
+            {name: 'Django', image: django},
+            {name: 'React', image: react},
+            {name: 'Vue', image: vue},
+            {name: 'Java', image: java},
+            {name: 'WordPress', image: wordpress},
+            {name: 'GitHub', image: github},
+        ],
         loading: true,
     }
 
@@ -17,7 +36,25 @@ export default class Projects extends React.Component{
       .then(res => {
         const projects = res.data;
         this.setState({ projects, loading: false, });
+        this.setState({filteredProjects: projects})
       })
+    }
+
+    filterProjects = (event) =>{
+        let filteredProjects;
+
+        if(event.target.value === 'All'){
+            filteredProjects = this.state.projects;
+        } else {
+            filteredProjects = this.state.projects.filter(item => {
+                let languagesUsed = item.languagesUsed.some(({name}) => name.includes(event.target.value))
+                return languagesUsed
+            });
+
+        }
+        this.setState({
+            filteredProjects: filteredProjects
+        })
     }
 
     render(){
@@ -40,9 +77,20 @@ export default class Projects extends React.Component{
                     <div className="projectsPurpleTopBackground" />
                     <p className="projectsTitle">Projects</p>
                     <p className="projectsSubtitle">Take a look at what I've been working on.</p>
+                    <div className="buttonsFilter">
+                        {this.state.languagesButtons.map((btn, index) =>{
+                            return(
+                                <div className="btns" >
+                                    <input type="image" alt="icon" src={btn.image} title={btn.name} key={index} value={btn.name} onClick={this.filterProjects} />
+                                </div>
+                            )
+                        })
+                        
+                        }                        
+                    </div>
                     <div className="projectsPurpleBottomBackground">
                         <div className="projectsListWrapper">
-                            {this.state.projects.map(project => 
+                            {this.state.filteredProjects.map(project => 
                                     <div className="projectsItem">
                                         <div className="imageBox">
                                         <Link to={`/projects/${project.slug}`}>
